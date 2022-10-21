096886f1e64b64200933c5d7c16f1360c1a2698b
Sara Vieira <hey@iamsaravieira.com>
2022-10-21 23:12:13 +0200

    add link to gh

--- utils/docs.js
+++ utils/docs.js
@@ -4,5 +4,5 @@ export const DOCS = `## docs
 - accepted image types: jpg/png/svg/gif
 - gif images will only render the first frame
 - use invert for images with a dark background
-- submit issues and feature ideas on github
+- submit issues and feature ideas on <a href="https://github.com/axodotdev/axii" class="text-axo-orange" target="_blank" rel="noopener">github</a>
 `;
