602524b80b593e5d66815fddebf0475dc6071a82
Ashley Williams <ashley666ashley@gmail.com>
2022-10-21 15:45:31 -0500

    doc: add gif to supported types

--- utils/docs.js
+++ utils/docs.js
@@ -1,7 +1,7 @@
 export const DOCS = `## docs
 
 - drag the image over the left of the screen
-- accepted image types: jpg/png/svg
+- accepted image types: jpg/png/svg/gif
 - gif images will only render the first frame
 - use invert for images with a dark background
 `;
